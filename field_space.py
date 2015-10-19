6a12c3f37d1d68bc25f2849afafcf31e46cd6f5e
Dillon Huff <dillonhuff@gmail.com>
2015-10-18 22:11:52 -0700

    FieldSpace fields are now actually allocated

--- field_space.py
+++ field_space.py
@@ -6,10 +6,16 @@ class FieldSpace():
         self.task_name = task_name
         self.field_ids = field_ids
 
+    def allocate_fields(self):
+        allocate_stmts = []
+        for field in self.field_ids:
+            allocate_stmts.append('allocator.allocate_field(sizeof(int),' + field + ')')
+        return allocate_stmts
+
     def init_code(self):
         field_decl_str = cpp_var('FieldSpace ' + self.name)
         creation_call = cpp_funcall('runtime->create_field_space', [], [cpp_var('ctx')])
         assign = cpp_assign(field_decl_str, creation_call)
         create_allocator = cpp_assign(cpp_var('FieldAllocator allocator'), cpp_funcall('runtime->create_field_allocator', [], [cpp_var('ctx'), cpp_var(self.name)]))
-        allocate = cpp_block([create_allocator])
+        allocate = cpp_block([create_allocator] + self.allocate_fields())
         return [assign, allocate]
